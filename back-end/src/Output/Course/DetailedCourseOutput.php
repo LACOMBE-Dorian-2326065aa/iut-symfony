b6a33213ef8a21b773a5a834b11afb7df6306b97
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2026-02-02 10:46:45 +0100

    feat: add DocumentOutput to DetailedCourseOutput for document management

--- back-end/src/Output/Course/DetailedCourseOutput.php
+++ back-end/src/Output/Course/DetailedCourseOutput.php
@@ -3,6 +3,7 @@
 namespace App\Output\Course;
 
 use App\Entity\Course;
+use App\Output\Document\DocumentOutput;
 use App\Output\ListOutput;
 use App\Output\Video\VideoOutput;
 use App\Repository\DocumentRepository;
@@ -14,13 +15,14 @@ class DetailedCourseOutput
     public int $id;
     public string $name;
     public ListOutput $videos;
-    public array $documents;
+    public ListOutput $documents;
 
     public function __construct(Course $course)
     {
         $this->id = $course->getId();
         $this->name = $course->getName();
         $this->videos = new ListOutput($course->getVideos(), VideoOutput::class);
+        $this->documents = new ListOutput($course->getDocuments(), DocumentOutput::class);
     }
 
     public function build()

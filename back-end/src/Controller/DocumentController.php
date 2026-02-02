0f49d841fd58876711b8cb7a3e586fdd6c51d843
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2026-02-02 11:20:15 +0100

    feat: add document upload endpoint to DocumentController

--- back-end/src/Controller/DocumentController.php
+++ back-end/src/Controller/DocumentController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Document;
 use App\Output\Document\DocumentOutput;
 use App\Output\ListOutput;
+use App\Repository\CourseRepository;
 use App\Repository\DocumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,7 +22,7 @@ final class DocumentController extends AbstractController
         $this->documentRepository = $documentRepository;
     }
 
-    #[Route('', name: 'document_list')]
+    #[Route('', name: 'document_list', methods: ['GET'])]
     public function getAllDocuments(): Response
     {
         $documents = $this->documentRepository->findAll();
@@ -27,5 +30,47 @@ final class DocumentController extends AbstractController
         return $this->json(
             new ListOutput($documents, DocumentOutput::class)
         );
+    }
+
+    #[Route('/upload', name: 'document_detail', methods: ['POST'])]
+    public function uploadDocument(Request $request, CourseRepository $courseRepository): Response
+    {
+        $content = $request->getContent();
+        $data = json_decode($content, true);
+
+        $file = $data['file'];
+        $fileContent = base64_decode($file);
+
+        $fileName = $data['name'];
+        $courseId = $data['courseId'];
+        $numberOfPages = $data['numberOfPages'];
+
+        $course = $courseRepository->find($courseId);
+        if (!$course) {
+            return $this->json(['error' => 'Course not found'], 404);
+        }
+
+        if (!$file) {
+            return $this->json(['error' => 'No file provided'], 400);
+        }
+
+        $uploadDir = $this->getParameter('upload_dir') . '/' . $courseId;
+
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        file_put_contents(
+            $uploadDir . '/' . $fileName . '.pdf',
+            $fileContent
+        );
+
+        $newDocument = new Document();
+        $newDocument->setName($fileName)
+            ->setCourse($course)
+            ->setPath($fileName . '.pdf')
+            ->setNumberOfPages($numberOfPages);
+
+        return $this->json(['message' => 'Document upload endpoint']);
     }
 }

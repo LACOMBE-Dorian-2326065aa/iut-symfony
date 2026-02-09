5ad3f9b769f808c9e73dd5cc9ef6aa40e8c27bda
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2026-02-09 08:41:00 +0100

    fix: add user output to DetailedCourseOutput

--- back-end/src/Output/Course/DetailedCourseOutput.php
+++ back-end/src/Output/Course/DetailedCourseOutput.php
@@ -6,6 +6,7 @@ use App\Entity\Course;
 use App\Output\Document\DocumentOutput;
 use App\Output\ListOutput;
 use App\Output\Quizz\QuizzOutput;
+use App\Output\User\UserOutput;
 use App\Output\Video\VideoOutput;
 use App\Repository\DocumentRepository;
 use Doctrine\ORM\Mapping as ORM;
@@ -16,6 +17,7 @@ class DetailedCourseOutput extends CourseOutput
     public ListOutput $videos;
     public ListOutput $documents;
     public ListOutput $quizzs;
+    public UserOutput $user;
 
     public function __construct(Course $course)
     {
@@ -23,6 +25,7 @@ class DetailedCourseOutput extends CourseOutput
         $this->videos = new ListOutput($course->getVideos(), VideoOutput::class);
         $this->documents = new ListOutput($course->getDocuments(), DocumentOutput::class);
         $this->quizzs = new ListOutput($course->getQuizzs(), QuizzOutput::class);
+        $this->user = new UserOutput($course->getUser());
     }
 
     public function build()
